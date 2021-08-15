8abcdc3d86fc9052ae62741453ecb5d85c4a32a7
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-15 17:47:40 -0400

    Completed CRUD methods for EmployeeController

--- src/main/java/io/zipcoder/persistenceapp/EmployeeController.java
+++ src/main/java/io/zipcoder/persistenceapp/EmployeeController.java
@@ -3,10 +3,9 @@ package io.zipcoder.persistenceapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/API/employee-controller")
@@ -18,5 +17,23 @@ public class EmployeeController {
     @PostMapping(value = "/create")
     public ResponseEntity<Employee> create(@RequestBody Employee employee){
         return new ResponseEntity<>(service.create(employee), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/read{id}")
+    public ResponseEntity<Employee> read(@PathVariable Long id){
+        return new ResponseEntity<>(service.read(id), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/read")
+    public ResponseEntity<List<Employee>> readAll() { return new ResponseEntity<>(service.readAll(), HttpStatus.OK); }
+
+    @PutMapping(value = "/update{id}")
+    public ResponseEntity<Employee> update(@PathVariable Long id, @RequestBody Employee newEmployeeData){
+        return new ResponseEntity<>(service.update(id, newEmployeeData), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<Employee> delete(@PathVariable Long id){
+        return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
