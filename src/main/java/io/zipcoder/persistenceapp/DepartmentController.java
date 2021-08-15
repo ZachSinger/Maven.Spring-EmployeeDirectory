368fff7b831638218c9d424e21098ad2f82631a0
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-15 18:09:32 -0400

    Completed DepartmentController CRUD methods

--- src/main/java/io/zipcoder/persistenceapp/DepartmentController.java
+++ src/main/java/io/zipcoder/persistenceapp/DepartmentController.java
@@ -3,10 +3,9 @@ package io.zipcoder.persistenceapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/API/department-controller")
@@ -18,5 +17,23 @@ public class DepartmentController {
     @PostMapping(value = "/create")
     public ResponseEntity<Department> create(@RequestBody Department department){
         return new ResponseEntity<>(service.create(department), HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/read")
+    public ResponseEntity<List<Department>> readAll(){ return new ResponseEntity<>(service.readAll(), HttpStatus.OK); }
+
+    @GetMapping(value = "/read/{id}")
+    public ResponseEntity<Department> read(@PathVariable Long id){
+        return new ResponseEntity<>(service.read(id), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public ResponseEntity<Department> update(@PathVariable Long id, @RequestBody Department department){
+        return new ResponseEntity<>(service.update(id, department), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<Department> delete (@PathVariable Long id){
+            return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
 }
